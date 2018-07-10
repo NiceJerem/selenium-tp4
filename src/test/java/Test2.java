16b2348e3e1e9e0c27abc7c30dae3b65f1e260c1
unknown <Nicejerem@hotmail.fr>
2018-07-10 15:13:24 +0200

    ajout du premier test

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -1,2 +1,16 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test2 {
+
+    WebDriver driver;
+    @Test
+    public void t1()
+    {
+        driver = new ChromeDriver();
+        driver.get("http://www.google.fr");
+        driver.quit();
+    }
 }
+
