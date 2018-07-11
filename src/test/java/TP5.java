a2bfafa593197f0df1d77c6278dbed85776f6919
unknown <Nicejerem@hotmail.fr>
2018-07-11 10:12:06 +0200

    Ajout du PageObject

--- src/test/java/TP5.java
+++ src/test/java/TP5.java
@@ -1,2 +1,48 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TP5 {
+    WebDriver driver;
+
+    @Before
+    public void beforeTest()
+    {
+        driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+    }
+
+    @Test
+    public void t1()
+    {
+        WebElement barre= driver.findElement(By.name("q"));
+        barre.sendKeys("france"+ Keys.ENTER);
+    }
+
+    @Test
+    public void t2()
+    {
+        GooglePage page = new GooglePage(driver);
+        page.FindWithBtn("france");
+    }
+
+    @Test
+    public void t3()
+    {
+        GooglePage page = new GooglePage(driver);
+        page.FindWithEnter("france");
+    }
+
+    @After
+    public void tearDown()
+    {
+
+    }
+
 }
+
--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
@@ -28,21 +30,13 @@ public class Test2 {
     @Test
     public void t1()
     {
-        /*
-        3.	Ouvrir la categorie Jeux
-        4.	Dans la liste de resultats, ouvrir le N1 de ventes (Mario Kart)
-        5.	Ajouter le jeu au panier
-        6.	Ouvrir le panier
-        7.	Changer la quantité a 2
-        8.	Vérifier que le prix est de EUR 99,98
-        */
         String idDivMenu="nav-link-shopall";
         WebElement divMenu =  driver.findElement(By.id(idDivMenu));
         Actions actions = new Actions(driver);
         actions.moveToElement(divMenu);
         WebDriverWait wait = new WebDriverWait(driver,5);
         actions.build().perform();
-        WebElement span = driver.findElement(By.cssSelector("span[data-nav-panelkey=\"VideogamesConsolesPanel\"]"));//data-nav-panelkey="VideogamesConsolesPanel"
+        WebElement span = driver.findElement(By.cssSelector("span[data-nav-panelkey=\"VideogamesConsolesPanel\"]"));
         wait.until(ExpectedConditions.visibilityOf(span));
         actions.moveToElement(span);
         actions.build().perform();
@@ -65,28 +59,37 @@ public class Test2 {
         btnAddToCart.click();
 
         //hlb-view-cart-announce
-        WebElement btnViewCart =  driver.findElement(By.id("hlb-view-cart-announce"));
-        btnViewCart.click();
+         driver.findElement(By.id("hlb-view-cart-announce")).click();
 
-        WebElement comboQte =  driver.findElement(By.id("a-autoid-0"));
-        comboQte.click();
+        driver.findElement(By.id("a-autoid-0")).click();;
+
         WebElement drop = driver.findElement(By.id("a-popover-1"));//By.xpath("//a[text()=\"2\"]"));
-        //WebElement qte2 = drop.findElement(By.id("dropdown2_1"));//////a[data-value="{"stringVal":"2"}"]
         String hidden= drop.getAttribute("aria-hidden");
         System.out.println("hidden :"+hidden);
-        WebElement qte2 = drop.findElement(By.linkText("2"));
-        //data-value="{"stringVal":"2"}"
-        actions.moveToElement(qte2);
-        actions.click();
-        actions.build().perform();
-
+        //-----------------------------
+        // Bon Quand meme
+        //WebElement qte2 = drop.findElement(By.linkText("2"));
+        //actions.moveToElement(qte2).click();;
+        //actions.build().perform();
+        //-----------------------------
+        Select quantitySelect = new Select(driver.findElement(By.name("quantity")));
+        quantitySelect.selectByValue("2");
+        //-----------
         WebElement result =  driver.findElement(By.id("sc-subtotal-amount-activecart"));
         System.out.println(result.getText());
         wait.until(ExpectedConditions.textToBe(By.id("sc-subtotal-amount-activecart"),"EUR 99,98"));
         result =  driver.findElement(By.id("sc-subtotal-amount-activecart"));
-        Assert.assertThat(result.getText(),Is.is("EUR 99,98"));
-        System.out.println("OK");
+        //------------------------------------------------------------------------------
+        FluentWait<WebDriver> fwait = new FluentWait<WebDriver>(driver);
 
+        fwait.until(driver ->{
+            String sousTotal = driver.findElement(By.id("sc-subtotal-amount-activecart")).getText();
+            return sousTotal.contains("(2 articles):");
+        });
+
+        Assert.assertThat(result.getText(),Is.is("EUR 99,98"));
+        System.out.println(result.getText());
+        System.out.println("OK");
 
     }
 
